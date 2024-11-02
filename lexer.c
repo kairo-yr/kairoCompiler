8512c3a3190352c8c2b34e89eeb8d32aab332a4a
kairo <kairobytes@gmail.com>
2024-11-02 16:19:18 +0530

    added code to handle \n and comments

--- lexer.c
+++ lexer.c
@@ -341,12 +341,77 @@ struct token *read_special_token()
     return NULL;
 }
 
+struct token* token_make_newline()
+{
+    nextc();
+    return token_create(&(struct token){.type=TOKEN_TYPE_NEWLINE});
+}
+
+struct token* token_make_one_line_comment()
+{
+    struct buffer* buffer=buffer_create();
+    char c=0;
+    LEX_GETC_IF(buffer,c,(c!='\n' && c!=EOF));
+
+    return token_create(&(struct token){.type=TOKEN_TYPE_COMMENT});
+}
+
+struct token* token_make_multiline_comment()
+{
+    struct buffer* buffer=buffer_create();
+    char c=0;
+    while (1)
+    {
+        LEX_GETC_IF(buffer,c,(c!='*' && c!=EOF));
+        if(c==EOF)
+        {
+            compiler_error(lex_process->compiler,"You did not close this multiline comment\n");
+        }
+        else if(c=='*')
+        {
+            nextc();
+            if(peekc()=='/')
+            {
+                nextc();
+                break; 
+            }
+        }
+    }
+    return token_create(&(struct token){.type=TOKEN_TYPE_COMMENT,.sval=buffer_ptr(buffer)});
+}
+
+struct token* handle_comment()
+{
+    char c = peekc();
+    if(c == '/')
+    {
+        nextc();
+        if(peekc() == '/')
+        {
+            nextc();
+            return token_make_one_line_comment();
+        }
+        else if(peekc() == '*')
+        {
+            nextc();
+            return token_make_multiline_comment();
+        }
+        pushc('/');
+        return token_make_operator_or_string();
+    }
+    return NULL;
+}
 struct token *read_next_token()
 {
     struct token *token = NULL;
     char c = peekc();
     // printf("Read character: '%c'\n", c);
 
+    token = handle_comment();
+    if(token)
+    {
+        return token;
+    }
     switch (c)
     {
     NUMERIC_CASE:
@@ -369,6 +434,9 @@ struct token *read_next_token()
         token = handle_whitespace();
         break;
 
+    case '\n':
+        token = token_make_newline();
+        break;
     case '$':
         // we have finished  lexical analysis on the file
         break;
